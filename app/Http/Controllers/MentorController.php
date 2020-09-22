316bcdbe4e3fa5b60940882441f1828d77f66855
AamDsam <muh.muammar@gmail>
2020-09-22 23:33:22 +0800

    update mentors

--- app/Http/Controllers/MentorController.php
+++ app/Http/Controllers/MentorController.php
@@ -22,7 +22,7 @@ class MentorController extends Controller
         $validator = Validator::make($data,$rules);
 
         if ($validator->fails()){
-            return response()->json([
+            return response()->json([  
                 'status' => 'error',
                 'message' => $validator->errors()
             ], 400);
@@ -30,6 +30,40 @@ class MentorController extends Controller
 
         $mentor = Mentor::create($data);
 
+        return response()->json(['status' => 'success', 'data' => $mentor]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'string',
+            'profile' => 'url',
+            'profession' => 'string',
+            'email' => 'email'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::find($id);
+        if (!$mentor){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found',
+            ],404);
+        }
+
+        $mentor->fill($data); 
+
+        $mentor->save(); 
         return response()->json(['status' => 'success', 'data' => $mentor]);
     }
 }
