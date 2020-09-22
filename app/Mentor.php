5b7e61bca4ed7652c5b0efb898315e9f12f8ae90
AamDsam <muh.muammar@gmail>
2020-09-22 22:52:13 +0800

    create mentor

--- app/Mentor.php
+++ app/Mentor.php
@@ -8,6 +8,11 @@ class Mentor extends Model
 {
     protected $table = 'mentors';
 
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
+
     protected $fillable = [
         'name', 'profile', 'email', 'profession'
     ];
